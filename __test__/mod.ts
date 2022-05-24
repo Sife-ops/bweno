f52dbaf27525253bdddfee3d2ec7311c77d88a14
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-05-24 01:39:23 -0700

    finally good

--- __test__/mod.ts
+++ __test__/mod.ts
@@ -4,9 +4,37 @@ Deno.test({
   name: 'temp',
   fn: async () => {
     const b = new Bweno();
-    const r = await b.create.folder({
+
+    // const r = await b.create.login({
+    //   name: 'asdf',
+    //   login: {
+    //     username: 'a',
+    //     password: 'b',
+    //   },
+    // });
+
+    // const r = await b.create.folder({
+    //   name: 'asdf',
+    // });
+
+    // const r = await b.create.card({
+    //   name: 'asdf',
+    //   card: {
+    //     number: '123',
+    //   },
+    // });
+
+    // const r = await b.create.identity({
+    //   name: 'asdf',
+    //   identity: {
+    //     firstName: 'asdf',
+    //   },
+    // });
+
+    const r = await b.create.secureNote({
       name: 'asdf',
-    });
+    })
+
     console.log(r);
   },
 });
